c4f31adbedb787c4af314899cdbdcb1e9d9eaba6
AidanRiverstone <aidan.riverstone8459@gmail.com>
2022-06-25 16:33:28 +1000

    Finished Script, added extra comments

--- FileManagerBeta.py
+++ FileManagerBeta.py
@@ -1,4 +1,4 @@
-# Import Shutil(File Mover, Copier and Renamer), OS(Operating System Commands), Time(Displays the Time and Date) and Subprocess(Unknown Use)
+# Import Shutil(File Mover, Copier and Renamer), OS(Operating System Commands) and Subprocess(Unknown Use)
 import shutil
 import os
 import time
@@ -45,7 +45,7 @@ def Dirlist():
         i+1
 
 def Check():
-    fp=int(input('Check exsistence of \n1.File \n2.Directory\n'))
+    fp=int(input('Check existence of \n1.File \n2.Directory\n'))
     if fp==1:
         path=input("Enter the file path:")
         os.path.isfile(path)
@@ -80,15 +80,78 @@ def Copy():
     shutil.copy(path1,path2)
     print('File Copied')
 
-def Rename():
-    path=input('Enter the path of the file to rename:')
-    name=input('Enter new name for file:')
-    shutil.rename(path,name)
-
 def Mkdir():
     path=input("Enter the directory name with path to make \neg. C:\\Hello\\Newdir \nWhere Newdir is new directory:")
     os.makedirs(path)
     print('Directory Created')
 
 def Rmdir():
-    
+    path=input('Enter path of Directory to delete:')
+    treedir=int(input('1.Deleted Directory \n2.Delete Directory Tree \n3.Exit \n'))
+    if treedir==1:
+        os.rmdir(path)
+    if treedir==2:
+        shutil.rmtree(path)
+        print('Directory Deleted')
+    if treedir==3:
+        exit()
+def OpenFile():
+    path=input('Enter the path of program:')
+    try:
+        os.startfile(path)
+    except:
+        print('File Not Found')
+
+# Clears the screen, and gives the user their options on what to do
+run=1
+while(run==1):
+    try:
+        os.system('clear')
+    except OSError:
+        os.system('cls')
+    print('\n>>>>>>>>>>>Python File Manager<<<<<<<<<<\n')
+    print('\nChoose the option number: \n')
+    dec=int(input('''1.Read a file
+2.Write to a file
+3.Add to a file
+4.Delete a file
+5.List files in a directory
+6.Check file existence
+7.Move a file
+8.Copy a file
+9.Create a directory
+10.Delete a directory
+11.Open a program
+12.Exit
+
+'''))
+    # Determines what will happen based on what the user inputed
+    if dec==1:
+        Read()
+    if dec==2:
+        Write()
+    if dec==3:
+        Add()
+    if dec==4:
+        Delete()
+    if dec==5:
+        Dirlist()
+    if dec==6:
+        Check()
+    if dec==7:
+        Move()
+    if dec==8:
+        Copy()
+    if dec==9:
+        Mkdir()
+    if dec==10:
+        Rmdir()
+    if dec==11:
+        OpenFile()
+    if dec==12:
+        exit()
+    else:
+        print('Error: Unknown Request')
+    run=int(input("1.Return to menu\n2.Exit \n"))
+    if run==2:
+        exit()
